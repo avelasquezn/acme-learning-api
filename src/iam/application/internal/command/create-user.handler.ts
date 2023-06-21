c817d34797a8c23d23ab48954bba9708535e97ca
Angel Velasquez <pcisavel@upc.edu.pe>
2023-06-20 20:16:11 -0500

    feat: added user command handler.

--- src/iam/application/internal/command/create-user.handler.ts
+++ src/iam/application/internal/command/create-user.handler.ts
@@ -1,17 +1,25 @@
 import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import { CreateUserCommand } from '../../../domain/model/commands/create-user.command';
 import { User } from '../../../domain/model/aggregates/user.entity';
+import { Inject } from '@nestjs/common';
+import { UserRepository } from '../../../infrastructure/repositories/user.repository';
 
 @CommandHandler(CreateUserCommand)
 export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
-  constructor(private publisher: EventPublisher) {}
+  constructor(
+    @Inject('USER_REPOSITORY')
+    private repository: UserRepository,
+    private publisher: EventPublisher,
+  ) {}
   async execute(command: CreateUserCommand) {
     const { username, email, password } = command;
     let user = new User();
     user.username = username;
     user.email.address = email;
     user.password = password;
-    // TODO: save new user
+    user = this.publisher.mergeObjectContext(
+      await this.repository.persist(user),
+    );
     user.onCreation();
     user.commit();
   }
